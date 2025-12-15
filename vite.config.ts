b1159e869f40540727466fccd45d2ef6be610a90
nyanSpruk <nik.janspruk@gmail.com>
2025-12-15 16:21:37 +0100

    Setup of shadcn and tailwind and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      ui: path.resolve(__dirname, "./src/components/ui"),
+      lib: path.resolve(__dirname, "./src/lib"),
+      utils: path.resolve(__dirname, "./src/lib/utils"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      interfaces: path.resolve(__dirname, "./src/interfaces"),
+      data: path.resolve(__dirname, "./src/data"),
+      pages: path.resolve(__dirname, "./src/pages"),
+    },
+  },
+});
